bea3a9c8d70de6329f4febec82a39f46bae96c7b
lijiaying@ubuntuOnMac <lijiaying1989@gmail.com>
2016-10-13 15:13:31 +0800

    regular update of development on Linux

---
+++ test/afnp2014_true.cpp
@@ -0,0 +1,25 @@
+#include "iif.h"
+#include <iostream>
+using namespace iif;
+
+int loopFunction(int _reserved_input_[]) {
+int x = _reserved_input_[0];
+int y = _reserved_input_[1];
+
+iif_assume(x == 1 && y == 0);
+while(rand() % 8)
+{
+recordi(x, y);
+ x = x + y; y = y + 1;
+}
+recordi(x, y);
+iif_assert(x >= y);
+return 0;
+}
+
+int main(int argc, char** argv)
+ {
+iifContext context("/home/parallels/Research/GitHub/ZILU/tmp/afnp2014_true.var", loopFunction, "loopFunction", "/home/parallels/Research/GitHub/ZILU/tmp/afnp2014_true.ds");
+context.addLearner("conjunctive");
+return context.learn("/home/parallels/Research/GitHub/ZILU/tmp/afnp2014_true.cnt", "/home/parallels/Research/GitHub/ZILU/tmp/afnp2014_true");
+}
